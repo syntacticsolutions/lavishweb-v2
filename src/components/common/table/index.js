36b59ab160eb71428e4b333319d956fb8a5b26ab
Miguel Coder <syntacticsolutions@gmail.com>
2021-06-02 19:38:27 -0700

    Created reusable table component

---
+++ src/components/common/table/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+export const Table = ({data, configs, additionalData, maxHeight}) => {
+    const headers = configs.map(({title}) => title)
+
+
+    return (
+        <div className="lavish-table" style={{maxHeight}}>
+            <table border="0">
+                <thead>
+                    {headers.map((header) => (
+                        <th>{header}</th>
+                    ))}
+                </thead>
+                <tbody>
+                    {data.map((row) =>
+                        <tr>
+                            {configs.map((config) => (
+                                <>
+                                    {
+                                        config.content ?
+                                            <td>{config.content({ data, additionalData })}</td> :
+                                            <td>{row?.[config.key]}</td>
+                                    }
+                                </>
+                            ))}
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        </div>
+    )
+}
